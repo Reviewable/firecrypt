3d83b30820d97b31a535dbe450d51dc8ba12c14b
Piotr Kaminski <piotr@ideanest.com>
2025-11-29 22:17:31 -0800

    Decrypt old values even if new ones are no longer encrypted.
    The previous bound function comparison would always fail and fall
    through to the generic path, which would only trigger decryption if
    the rules specified that the path was encrypted.  This worked fine
    in practice but would fail if a previously encrypted path ever
    became non-encrypted.

--- src/crypto.js
+++ src/crypto.js
@@ -111,22 +111,21 @@ export default class Crypto {
       );
     }
     try {
-      const transform =
-        transformType === 'encrypt' ? this.encrypt.bind(this) : this.decrypt.bind(this);
-      return this.transformTree(value, this.specForPath(path), transform);
+      return this._transformTree(value, this.specForPath(path), transformType);
     } catch (e) {
       if (e.firecrypt) e.firecryptPath = path;
       throw e;
     }
   }
 
-  transformTree(value, def, transform) {
+  _transformTree(value, def, transformType) {
+    // transformType is either 'encrypt' or 'decrypt'.
     if (!def) return value;
     const type = this.getType(value);
     let i;
     if (/^(string|number|boolean)$/.test(type)) {
       if (def['.encrypt'] && def['.encrypt'].value) {
-        value = transform(value, type, def['.encrypt'].value);
+        value = this[transformType](value, type, def['.encrypt'].value);
       }
     } else if (type === 'object' && value !== null) {
       const transformedValue = {};
@@ -138,32 +137,34 @@ export default class Crypto {
           const keyParts = key.split('/');
           subDef = def;
           for (i = 0; i < keyParts.length; i++) {
-            if (transform === this.decrypt) {
+            if (transformType === 'decrypt') {
               keyParts[i] = this.decrypt(keyParts[i]);
               subDef = subDef && (subDef[keyParts[i]] || subDef.$);
             } else {
               subDef = subDef && (subDef[keyParts[i]] || subDef.$);
               if (subDef && subDef['.encrypt'] && subDef['.encrypt'].key) {
-                keyParts[i] = transform(keyParts[i], 'string', subDef['.encrypt'].key);
+                keyParts[i] = this[transformType](keyParts[i], 'string', subDef['.encrypt'].key);
               }
             }
           }
           key = keyParts.join('/');
-        } else if (transform === this.decrypt) {
+        } else if (transformType === 'decrypt') {
           key = this.decrypt(key);
           subDef = def[key] || def.$;
         } else {
           subDef = def[key] || def.$;
           if (subDef && subDef['.encrypt'] && subDef['.encrypt'].key) {
-            key = transform(key, 'string', subDef['.encrypt'].key);
+            key = this[transformType](key, 'string', subDef['.encrypt'].key);
           }
         }
-        transformedValue[key] = this.transformTree(subValue, subDef, transform);
+        transformedValue[key] = this._transformTree(subValue, subDef, transformType);
       }
       value = transformedValue;
     } else if (type === 'array') {
       if (!def.$) return value;
-      for (i = 0; i < value.length; i++) value[i] = this.transformTree(value[i], def.$, transform);
+      for (i = 0; i < value.length; i++) {
+        value[i] = this._transformTree(value[i], def.$, transformType);
+      }
     }
     return value;
   }
