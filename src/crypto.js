b8951b78e18d2a0dd3fa5a19e8338e21cd62df79
Piotr Kaminski <piotr@ideanest.com>
2025-12-02 22:50:37 -0800

    Implement automatic string value compression.
    And update the build toolchain while at it.

--- src/crypto.js
+++ src/crypto.js
@@ -1,10 +1,37 @@
+import FireCryptError from './FireCryptError';
+
 export default class Crypto {
   constructor(options, spec) {
     this._spec = this._cleanSpecification(spec);
-    this._encryptString = this._throwNotSetUpError;
-    this._decryptString = this._throwNotSetUpError;
-
     this._patternRegexes = {};
+    this.stats = {
+      compression: {attempts: 0, thresholdAccuracy: 0, bytesIn: 0, bytesOut: 0, ratio: 0}
+    };
+
+    switch (options.compression) {
+      case 'deflate':
+        this._compress = str => {
+          const inputU8 = fflate.strToU8(str);
+          const outputU8 = fflate.deflateSync(inputU8, {level: 9, ...options.compressionOptions});
+          const reduced = inputU8.byteLength > outputU8.byteLength;
+          const stats = this.stats.compression;
+          stats.thresholdAccuracy =
+            (stats.thresholdAccuracy * stats.attempts + reduced) / ++stats.attempts;
+          if (reduced) {
+            stats.bytesIn += inputU8.byteLength;
+            stats.bytesOut += outputU8.byteLength;
+            stats.ratio = stats.bytesOut / stats.bytesIn;
+          }
+          return reduced ? outputU8 : str;
+        };
+        break;
+      case 'none':
+        break;
+      default:
+        throw new FireCryptError(
+          `Unknown compression algorithm "${options.compression}".`, 'BAD_CONFIG');
+    }
+    this._compressionThreshold = options.compressionThreshold || 150;
 
     if (typeof LRUCache === 'function') {
       this._encryptionCache = new LRUCache({
@@ -16,6 +43,13 @@ export default class Crypto {
         length: this._computeCacheItemSize,
       });
     }
+
+    if (typeof Buffer !== 'undefined') {
+      /* eslint-disable no-undef */
+      this._base64UrlFromU8 = u8 => Buffer.from(u8).toString('base64url');
+      this._base64UrlToU8 = str => Buffer.from(str, 'base64url');
+      /* eslint-enable no-undef */
+    }
   }
 
   _cleanSpecification(def, path) {
@@ -25,35 +59,34 @@ export default class Crypto {
         const encryptKeys = Object.keys(def[key]);
         for (const encryptKey of encryptKeys) {
           if (encryptKey !== 'key' && encryptKey !== 'value' && encryptKey !== 'few') {
-            throw new Error(`Illegal .encrypt subkey: ${encryptKey}`);
+            throw new FireCryptError(`Illegal .encrypt subkey: ${encryptKey}`, 'BAD_SPEC');
           }
         }
       } else {
         // eslint-disable-next-line no-control-regex
         if (/[\x00-\x1f\x7f\x91\x92.#[\]/]/.test(key) || /[$]/.test(key.slice(1))) {
-          throw new Error(`Illegal character in specification key: ${key}`);
+          throw new FireCryptError(`Illegal character in specification key: ${key}`, 'BAD_SPEC');
         }
         this._cleanSpecification(def[key], (path || '') + '/' + key);
       }
       switch (key.charAt(0)) {
         case '$':
           if (key === '$') break;
-          if (def.$) throw new Error('Multiple wildcard keys in specification at ' + path);
+          if (def.$) {
+            throw new FireCryptError(
+              `Multiple wildcard keys in specification at ${path}`, 'BAD_SPEC');
+          }
           def.$ = def[key];
           delete def[key];
           break;
         case '.':
-          if (key !== '.encrypt') throw new Error('Unknown directive at ' + path + ': ' + key);
+          if (key !== '.encrypt') {
+            throw new FireCryptError(`Unknown directive at ${path}: ${key}`, 'BAD_SPEC');
+          }
           break;
       }
     }
     return def;
-  }
-
-  _throwNotSetUpError() {
-    const e = new Error('Encryption not set up');
-    e.firecrypt = 'NO_KEY';
-    throw e;
   }
 
   _computeCacheItemSize(value, key) {
@@ -72,7 +105,7 @@ export default class Crypto {
       def = def[path[i]] || def.$;
       if (!def) break;
       if (def['.encrypt'] && def['.encrypt'].key) {
-        path[i] = this.encrypt(path[i], 'string', def['.encrypt'].key);
+        path[i] = this.encrypt(path[i], 'string', def['.encrypt'].key, false);
       }
     }
     return path;
@@ -85,15 +118,20 @@ export default class Crypto {
 
   decryptRef(ref) {
     const path = this.refToPath(ref, true);
-    let changed = false;
-    for (let i = 0; i < path.length; i++) {
-      const decryptedPathSegment = this.decrypt(path[i]);
-      if (decryptedPathSegment !== path[i]) {
-        path[i] = decryptedPathSegment;
-        changed = true;
+    try {
+      let changed = false;
+      for (let i = 0; i < path.length; i++) {
+        const decryptedPathSegment = this.decrypt(path[i]);
+        if (decryptedPathSegment !== path[i]) {
+          path[i] = decryptedPathSegment;
+          changed = true;
+        }
       }
+      return changed ? ref.root.child(path.join('/')) : ref;
+    } catch (e) {
+      if (e.firecrypt) e.firecryptPath = path.join('/');
+      throw e;
     }
-    return changed ? ref.root.child(path.join('/')) : ref;
   }
 
   specForPath(path, def) {
@@ -107,7 +145,8 @@ export default class Crypto {
   transformValue(path, value, transformType) {
     if (transformType !== 'encrypt' && transformType !== 'decrypt') {
       throw new Error(
-        `Transform type must be either "encrypt" or "decrypt", but got "${transformType}".`
+        `Internal error: transform type must be either "encrypt" or "decrypt", ` +
+        `but got "${transformType}".`
       );
     }
     try {
@@ -125,7 +164,7 @@ export default class Crypto {
     let i;
     if (/^(string|number|boolean)$/.test(type)) {
       if (def['.encrypt'] && def['.encrypt'].value) {
-        value = this[transformType](value, type, def['.encrypt'].value);
+        value = this[transformType](value, type, def['.encrypt'].value, true);
       }
     } else if (type === 'object' && value !== null) {
       const transformedValue = {};
@@ -143,7 +182,8 @@ export default class Crypto {
             } else {
               subDef = subDef && (subDef[keyParts[i]] || subDef.$);
               if (subDef && subDef['.encrypt'] && subDef['.encrypt'].key) {
-                keyParts[i] = this[transformType](keyParts[i], 'string', subDef['.encrypt'].key);
+                keyParts[i] =
+                  this[transformType](keyParts[i], 'string', subDef['.encrypt'].key, false);
               }
             }
           }
@@ -154,7 +194,7 @@ export default class Crypto {
         } else {
           subDef = def[key] || def.$;
           if (subDef && subDef['.encrypt'] && subDef['.encrypt'].key) {
-            key = this[transformType](key, 'string', subDef['.encrypt'].key);
+            key = this[transformType](key, 'string', subDef['.encrypt'].key, false);
           }
         }
         transformedValue[key] = this._transformTree(subValue, subDef, transformType);
@@ -175,47 +215,71 @@ export default class Crypto {
     const pathStr = decodeURIComponent(ref.toString().slice(root.toString().length));
     if (!encrypted && pathStr && pathStr.charAt(0) !== '.' &&
         /[\x00-\x1f\x7f\x91\x92.#$[\]]/.test(pathStr)) {  // eslint-disable-line no-control-regex
-      throw new Error(`Path contains invalid characters: ${pathStr}`);
+      throw new FireCryptError(`Path contains invalid characters: ${pathStr}`, 'BAD_PATH');
     }
     return pathStr.split('/');
   }
 
-  encrypt(value, type, pattern) {
+  encrypt(value, type, pattern, allowCompression) {
+    const shouldCompress =
+      pattern === '#' && allowCompression && type === 'string' && this._compress &&
+      value.length >= this._compressionThreshold;
+    if (!this._encryptString && !shouldCompress) return value;
     let cacheKey;
     if (this._encryptionCache) {
       cacheKey = type.charAt(0) + pattern + '\x91' + value;
       if (this._encryptionCache.has(cacheKey)) return this._encryptionCache.get(cacheKey);
     }
+    let typeCode = type.charAt(0).toUpperCase();
     let result;
     if (pattern === '#') {
-      result = this.encryptValue(value, type);
+      if (shouldCompress) {
+        const compressedValue = this._compress(value);
+        if (!this._encryptString && typeof compressedValue === 'string') return value;
+        if (this._encryptString) {
+          typeCode = 'E';
+          result = this.encryptValue(compressedValue, type);
+        } else {
+          typeCode = 'C';
+          result = this._base64UrlFromU8(compressedValue);
+        }
+      } else {
+        result = this.encryptValue(value, type);
+      }
+      if (result !== value) {
+        result = `\x91${typeCode}${result}\x92`;
+        if (this._encryptionCache) this._encryptionCache.set(cacheKey, result);
+      }
     } else {
       if (type !== 'string') {
-        throw new Error('Can\'t encrypt a ' + type + ' using pattern [' + pattern + ']');
+        throw new FireCryptError(`Can't encrypt a ${type} using pattern [${pattern}]`, 'BAD_VALUE');
       }
+      if (!this._encryptString) return value;
       const match = value.match(this.compilePattern(pattern));
       if (!match) {
-        throw new Error(
-          'Can\'t encrypt as value doesn\'t match pattern [' + pattern + ']: ' + value);
+        throw new FireCryptError(
+          `Can't encrypt as value doesn't match pattern [${pattern}]: ${value}`, 'BAD_VALUE');
       }
       let i = 0;
       result = pattern.replace(/[#.]/g, placeholder => {
         let part = match[++i];
-        if (placeholder === '#') part = this.encryptValue(part, 'string');
+        if (placeholder === '#') part = `\x91S${this.encryptValue(part, 'string')}\x92`;
         return part;
       });
     }
-    if (this._encryptionCache) this._encryptionCache.set(cacheKey, result);
     return result;
   }
 
   encryptValue(value, type) {
-    if (!/^(string|number|boolean)$/.test(type)) throw new Error('Can\'t encrypt a ' + type);
+    if (!/^(string|number|boolean)$/.test(type)) {
+      throw new FireCryptError(`Can't encrypt a ${type}`, 'BAD_VALUE');
+    }
+    if (!this._encryptString) return value;
     switch (type) {
       case 'number': value = '' + value; break;
       case 'boolean': value = value ? 't' : 'f'; break;
     }
-    return '\x91' + type.charAt(0).toUpperCase() + this._encryptString(value) + '\x92';
+    return this._encryptString(value);
   }
 
   decrypt(value) {
@@ -226,34 +290,68 @@ export default class Crypto {
     let result;
     const match = value.match(/^\x91(.)([^\x92]*)\x92$/);
     if (match) {
-      const decryptedString = this._decryptString(match[2]);
+      if (match[1] !== 'C' && !this._decryptString) {
+        throw new FireCryptError('Unable to decrypt value because encryption turned off', 'NO_KEY');
+      }
       switch (match[1]) {
-        case 'S':
-          result = decryptedString;
+        case 'C':  // compressed, not encrypted string
+          result = fflate.strFromU8(fflate.decompressSync(this._base64UrlToU8(match[2])));
           break;
-        case 'N':
-          result = Number(decryptedString);
+        case 'E':  // compressed, encrypted string
+          result = fflate.strFromU8(fflate.decompressSync(
+            this._wordsToU8(this._decryptString(match[2], false))));
+          break;
+        case 'S':  // encrypted string
+          result = this._decryptString(match[2], true);
+          break;
+        case 'N': {  // encrypted number
+          const decryptionResult = this._decryptString(match[2], true);
+          result = Number(decryptionResult);
           // Check for NaN, since it's the only value where x !== x.
           // eslint-disable-next-line no-self-compare
-          if (result !== result) throw new Error(`Invalid encrypted number: ${decryptedString}`);
+          if (result !== result) {
+            throw new FireCryptError(`Invalid encrypted number: ${decryptionResult}`, 'BAD_VALUE');
+          }
           break;
-        case 'B':
-          if (decryptedString === 't') result = true;
-          else if (decryptedString === 'f') result = false;
-          else throw new Error('Invalid encrypted boolean: ' + decryptedString);
+        }
+        case 'B': {  // encrypted boolean
+          const decryptionResult = this._decryptString(match[2], true);
+          switch (decryptionResult) {
+            case 't': result = true; break;
+            case 'f': result = false; break;
+            default:
+              throw new FireCryptError(
+                `Invalid encrypted boolean: ${decryptionResult}`, 'BAD_VALUE');
+          }
           break;
+        }
         default:
-          throw new Error('Invalid encrypted value type code: ' + match[1]);
+          throw new Error(`Internal error: invalid encrypted value type code: ${match[1]}`);
       }
     } else {
+      if (!this._decryptString) {
+        throw new FireCryptError('Unable to decrypt value because encryption turned off', 'NO_KEY');
+      }
       result = value.replace(/\x91(.)([^\x92]*)\x92/g, (ignored, typeCode, encryptedString) => {
-        if (typeCode !== 'S') throw new Error('Invalid multi-segment encrypted value: ' + typeCode);
-        return this._decryptString(encryptedString);
+        if (typeCode !== 'S') {
+          throw new Error(`Internal error: invalid multi-segment encrypted value: ${typeCode}`);
+        }
+        return this._decryptString(encryptedString, true);
       });
     }
     if (this._decryptionCache) this._decryptionCache.set(value, result);
     return result;
   }
+
+  _wordsToU8(wordArray) {
+    const {sigBytes, words} = wordArray;
+    const uint8Array = new Uint8Array(sigBytes);
+    for (let i = 0; i < sigBytes; i++) {
+      // eslint-disable-next-line no-bitwise
+      uint8Array[i] = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff;
+    }
+    return uint8Array;
+  };
 
   getType(value) {
     if (Array.isArray(value)) return 'array';
@@ -275,5 +373,20 @@ export default class Crypto {
         .replace(/#/g, '(.*?)') + '$');
     }
     return regex;
+  }
+
+  _base64UrlFromU8(bytes) {
+    return btoa(Array.from(bytes, b => String.fromCharCode(b)).join(''))
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=+$/, '');
+  }
+
+  _base64UrlToU8(b64url) {
+    return Uint8Array.from(atob(
+      b64url.replace(/-/g, '+')
+        .replace(/_/g, '/')
+        .padEnd(Math.ceil(b64url.length / 4) * 4, '=')
+    ), c => c.charCodeAt(0));
   }
 }
